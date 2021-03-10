ec6abb776bc9914f731d8756768e6255f4a0919f
Dhawkins96 <57950733+Dhawkins96@users.noreply.github.com>
2021-03-10 14:09:17 +0000

    deleted unneeded classes adding help and about pages

---
+++ cleanTable_Mobile/cleanTable_Mobile/ViewModels/HelpViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace cleanTable_Mobile.ViewModels
+{
+    class HelpViewModel : BaseViewModel
+    {
+        public HelpViewModel()
+        {
+            Title = "Help";
+        }
+        public ICommand OpenWebCommand { get; }
+    }
+
+}
