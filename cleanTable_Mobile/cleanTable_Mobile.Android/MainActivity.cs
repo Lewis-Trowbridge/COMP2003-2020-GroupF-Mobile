e7b710aec389f8eca27bf91b4f97b9c6b5cfeeaa
Lewis Trowbridge <lewbob9927@gmail.com>
2021-04-21 19:01:53 +0100

    Altered application name and icon

--- cleanTable_Mobile/cleanTable_Mobile.Android/MainActivity.cs
+++ cleanTable_Mobile/cleanTable_Mobile.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace cleanTable_Mobile.Droid
 {
-    [Activity(Label = "cleanTable_Mobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "CleanTable", Icon = "@drawable/cleantable_logo", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
